ffdd5b6f988a362be8aaecf455bcaa423d63ee73
Pushpendra Vishwakarma <48829314+Pushpendrahpx@users.noreply.github.com>
2020-03-23 13:52:04 +0530

    Connection With DB is left, another part is done

---
+++ Models/ErrorMessage.js
@@ -0,0 +1,7 @@
+class ErrorMessage{
+    constructor(code,message){
+        this.code = code;
+        this.message = message;
+    }
+}
+module.exports = ErrorMessage;
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 5000;
-const router = express.Router;
+const router = express.Router();
+
+const config = require("./config.json"); // Configuration Files Containes Connection Details With Database
+
+const ErrorMessageClass = require("./Models/ErrorMessage");
 
 // Users Routes
 var USERS = require("./Routes/Users");
 app.use("/api/users",USERS);
 
+app.get("/",(req,res)=>{
+    console.log("Server Request Method in with GET '/' Method = "+req.secure)
+    res.status(400).json(new ErrorMessageClass(400,"This API Request is Invalid"));
+})
 app.listen(PORT,function(){
     console.log("Node Server Started At PORT="+PORT);
 })
