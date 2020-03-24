3d92fb25c66eca8b41fd3199af7b84f2e7eedcb0
Pushpendra Vishwakarma <48829314+Pushpendrahpx@users.noreply.github.com>
2020-03-24 18:30:18 +0530

    LOgin And Home

--- Routes/Users.js
+++ Routes/Users.js
@@ -78,7 +78,7 @@ router.post('/login',(req,res)=>{
                 }
 
                 try{
-                    f(users.password == password){
+                    if(users.password == password){
                         res.status(200).json({
                             statusCode:200,
                             success:"Logged In"
